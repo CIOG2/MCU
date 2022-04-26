8f23ed9947de16e0d5de98f49418fe86d9b8f854
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-04-26 09:57:10 -0600

    Header listo

---
+++ src/JS/components/Home/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    const logo = document.createElement("img");
+    logo.classList.add("Home__header--logo");
+    logo.src = "https://i.ibb.co/mCFzdsZ/Marvel-Logo.png";
+
+    const loginButton = document.createElement("button");
+    loginButton.classList.add("Home__header--login");
+    loginButton.textContent = "Iniciar Sesión";
+
+    const header = document.createElement("header");
+    header.classList.add("Home__header");
+    header.append( logo, loginButton);
+
+    return header;
+}
+
+export default Header;
---
+++ src/JS/index.js
@@ -0,0 +1,5 @@
+import Header from "./components/Home/Header.js";
+
+const app = document.getElementById("app");
+
+app.append(Header());

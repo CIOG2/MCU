559c57b2345805cf1145b85e1b4f68033f5e4547
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-04-27 15:14:08 -0600

    Skeleton listo

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,9 +1,22 @@
 import Skeleton from "./components/Skeleton/index.js";
+
 const app = document.getElementById("app");
 app.append(Skeleton());
+
+
 
 import HomeHeader from "./components/Home/Header.js";
 import HomeMain from "./components/Home/Main.js";
 
+setTimeout(() => {    
+    setTimeout(() => {
+        document.getElementById("container-skeleton").style.opacity = 0;        
+        setTimeout(() => {
+            document.getElementById("container-skeleton").remove();
+        }, 300);
+
+    }, 1000);
+    app.append(HomeHeader(), HomeMain());
+}, 1000);
 
 // app.append(HomeHeader(), HomeMain());
